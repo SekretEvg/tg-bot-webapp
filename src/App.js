fc7a21ae2b69a8057ccdd4a903151d39e2ef5e08
SekretEvg <sekret_pk@mail.ru>
2024-03-30 00:14:11 +0800

    add form, routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 import "./App.css";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
   const { tg, onToggleButton } = useTelegram();
@@ -13,6 +16,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import "./Form.css";
+import { useTelegram } from "../../hooks/useTelegram";
+
+const Form = () => {
+  const [country, setCountry] = useState("");
+  const [street, setStreet] = useState("");
+  const [subject, setSubject] = useState("physical");
+  const { tg } = useTelegram();
+
+  useEffect(() => {
+    if (!street || !country) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  }, [tg, country, street]);
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: "Отправить данные",
+    });
+  }, [tg]);
+
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
+  const onChangeStreet = (e) => {
+    setStreet(e.target.value);
+  };
+  const onChangeSubject = (e) => {
+    setSubject(e.target.value);
+  };
+
+
+
+  return (
+    <div className="form">
+      <h3>Введите ваши данные</h3>
+      <input
+        type="text"
+        className="input"
+        placeholder="Страна"
+        value={country}
+        onChange={onChangeCountry}
+      />
+      <input
+        type="text"
+        className="input"
+        placeholder="Улица"
+        value={street}
+        onChange={onChangeStreet}
+      />
+      <select className="select" value={subject} onChange={onChangeSubject}>
+        <option value="physical">Физ. лицо</option>
+        <option value="legal">Юр. лицо</option>
+      </select>
+    </div>
+  );
+};
+
+export default Form;
